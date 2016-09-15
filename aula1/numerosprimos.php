6370f8a8c07254dc2015be7b47ea6b44ddd98504
Marcos Mori <gigio0107@gmail.com>
2016-09-15 18:13:59 -0300

    numeros primos

---
+++ aula1/numerosprimos.php
@@ -0,0 +1,20 @@
+// sabado que vem
+// quero um php todos os numeros primos ate o maximo (esse numero sera uma variavel)
+// sao so dividios por um um por ele mesmo
+
+
+<?php 
+
+for ($n = 2; $n <= 100; $n++) { 
+$primo = true; 
+for ($x = 2; $x < $n; $x++) { 
+if ($n % $x == 0) { 
+$primo = false;
+break; 
+} 
+} 
+if ($primo) { 
+echo ' ' . $n; 
+} 
+} 
+?>
